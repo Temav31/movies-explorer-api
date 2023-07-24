59d83c11041a54a3bfb89cc0d150e11610dc1e51
Temav31 <temav331@mail.ru>
2023-07-24 22:59:08 +0300

    перед сдачей

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const errorHandler = require('./middlwares/error');
+const { requestLogger, errorLogger } = require('./middlwares/logger');
 // константы
 const app = express();
 app.use(express.json());
+app.use(cors({
+  credentials: true,
+  origin: [
+    'https://work.tema.nomoredomains.work',
+    'http://work.tema.nomoredomains.work',
+    'https://localhost:3000',
+    'http://localhost:3000',
+  ],
+}));
 const { PORT = 3000 } = process.env;
 // // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
@@ -14,7 +26,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
 });
 // роуты
 app.use(cookieParser());
+app.use(requestLogger);
+// обработка запросов
 app.use(router);
+// логер
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 // порт
